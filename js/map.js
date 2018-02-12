ea991ef72951225344ab95ade0112deaf45d8416
DrakDim <darsten.d@gmail.com>
2018-02-12 05:01:10 +0300

    Вынес указанные в заднии данные в константы

---
+++ js/map.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var PRICE_MIN = 1000;
+var PRICE_MAX = 1000000;
+
+var ROOMS_MIN = 1;
+var ROOMS_MAX = 5;
+
+var ARRAY_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var ARRAY_TYPES = ['flat', 'house', 'bungalo'];
+var ARRAY_TIMES = ['12:00', '13:00', '14:00'];
+var ARRAY_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var ARRAY_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var LOCATION_X_MIN = 300;
+var LOCATION_X_MAX = 900;
+var LOCATION_Y_MIN = 150;
+var LOCATION_Y_MAX = 500;
